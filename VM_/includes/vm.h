2e80c51020c3f6b76be1e8be4f1efacd166e91eb
Amric TRUDEL <atrudel@e2r6p16.42.fr>
2017-04-28 14:59:01 +0200

    created structures for the vm

--- VM_/includes/vm.h
+++ VM_/includes/vm.h
@@ -1,32 +1,38 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   vm.h                                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: atrudel <atrudel@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/04/25 19:19:34 by atrudel           #+#    #+#             */
-/*   Updated: 2017/04/26 17:06:35 by pdady            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef VM_H
 # define VM_H
 
 #include <fcntl.h>
 #include <stdio.h>
+#include "libft.h"
+#include "../../shared_files/op.h"
+
 typedef	struct		s_player
 {
 	char			*name;
 	int				nb_lives;
 }					t_player;
 
+typedef struct		s_process
+{
+	int				no;
+	int				player_no;
+	int				alive;
+	unsigned char	*registers;
+	unsigned char	*PC;
+	int				carry;
+}					t_process;
+
 typedef struct		s_vm
 {
 	unsigned char	*memory;
-	t_player		*players;
+	t_player		players[MAX_PLAYERS];
+	int				nb_players;
+	t_list			*processes;
 	int				cycle_nbr;
-	
 }					t_vm;
+
+void 				vm_error_exit(t_vm *vm, char *message);
+
 
 #endif
