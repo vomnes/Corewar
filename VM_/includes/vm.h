51d1f65b4b6301f34dc13d8247a3f40cecc79093
Amric TRUDEL <atrudel@e3r9p18.42.fr>
2017-04-28 17:41:26 +0200

    work on data sturctures, modified Makefile paths for op.h

--- VM_/includes/vm.h
+++ VM_/includes/vm.h
@@ -5,34 +5,38 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include "libft.h"
-#include "../../shared_files/op.h"
+#include "op.h"
 
-typedef	struct		s_player
+typedef	struct			s_player
 {
-	char			*name;
-	int				nb_lives;
-}					t_player;
+	char				name[PROG_NAME_LENGTH + 1];
+	char				comment[COMMENT_LENGTH + 1];
+	int					number;
+	int					nb_lives;
+}						t_player;
 
-typedef struct		s_process
+typedef struct			s_process
 {
-	int				no;
-	int				player_no;
-	int				alive;
-	unsigned char	*registers;
-	unsigned char	*PC;
-	int				carry;
-}					t_process;
+	int					no;
+	int					player_no;
+	int					alive;
+	unsigned char		registers[REG_NUMBER * REG_SIZE];
+	unsigned char		pc[REG_SIZE];
+	int					carry;
+	struct s_process	*next;
+	struct s_process	*prev;
+}						t_process;
 
-typedef struct		s_vm
+typedef struct			s_vm
 {
-	unsigned char	*memory;
-	t_player		players[MAX_PLAYERS];
-	int				nb_players;
-	t_list			*processes;
-	int				cycle_nbr;
-}					t_vm;
+	unsigned char		*memory;
+	t_player			players[MAX_PLAYERS + 1];
+	int					nb_players;
+	t_process			*processes;
+	int					cycle_nbr;
+}						t_vm;
 
-void 				vm_error_exit(t_vm *vm, char *message);
+void 					vm_error_exit(t_vm *vm, char *message);
 
 
 #endif
