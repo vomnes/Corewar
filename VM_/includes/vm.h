6fa072fe882167d616e79b9d84713ccca615d791
Amric TRUDEL <atrudel@e2r7p19.42.fr>
2017-04-30 15:21:34 +0200

    changed vm.h

--- VM_/includes/vm.h
+++ VM_/includes/vm.h
@@ -12,6 +12,7 @@ typedef	struct			s_player
 	char				name[PROG_NAME_LENGTH + 1];
 	char				comment[COMMENT_LENGTH + 1];
 	unsigned int		size_player;
+	unsigned char 		*player_memory;
 	int					number;
 	int					nb_lives;
 }						t_player;
@@ -24,6 +25,7 @@ typedef struct			s_process
 	unsigned char		registers[REG_NUMBER + 1][REG_SIZE];
 	unsigned char		pc[REG_SIZE];
 	int					carry;
+	t_instruction		instruction;
 	struct s_process	*next;
 	struct s_process	*prev;
 }						t_process;
@@ -37,17 +39,20 @@ typedef struct			s_vm
 	int					cycle_nbr;
 }						t_vm;
 
+
 typedef struct			s_instruction
 {
 	unsigned char		opcode;
-	void				(*op)(t_process *, t_vm *vm);
+	unsigned int		(*op)(t_process *, t_vm *);
+	unsigned int		cycles_to_execution;
 }						t_instruction;
+
 
 t_process				*vm_create_process(t_vm *vm);
 
 
 unsigned int			vm_n_bytes_to_uint(unsigned char *ptr, size_t n);
-unsigned int			vm_read_from_register(unsigned char *reg);
+unsigned int			vm_read_from_register(unsigned char reg[REG_SIZE]);
 void					vm_store_in_register(unsigned char (*reg)[REG_SIZE],
 						int value);
 
