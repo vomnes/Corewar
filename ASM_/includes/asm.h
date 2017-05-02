a422537722a6ec9ccbd4a3511471d7fc0a97c4d8
Valentin OMNES <vomnes@student.42.fr>
2017-05-02 13:32:01 +0200

    Finish manage/check name + comment | add ft_strtrim_comment + trim + delete comment in str

--- ASM_/includes/asm.h
+++ ASM_/includes/asm.h
@@ -6,7 +6,7 @@
 /*   By: vomnes <vomnes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 17:11:27 by vomnes            #+#    #+#             */
-/*   Updated: 2017/04/27 17:34:01 by vomnes           ###   ########.fr       */
+/*   Updated: 2017/05/02 13:15:22 by vomnes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,18 @@
 # include "../../libft/includes/get_next_line.h"
 # include "../../shared_files/op.h"
 
+typedef struct      s_parsing
+{
+    unsigned int    line_nb;
+    char            name_stocked;
+    char            comment_stocked;
+    char            *clean_line;
+}                   t_parsing;
+
 typedef struct s_input
 {
   int         fd;
-  char        file_name[129];
+  char        file_name[PROG_NAME_LENGTH + 1];
 }             t_input;
 
 typedef struct s_output
@@ -35,12 +43,14 @@ typedef struct  s_data
   header_t      header;
   t_input       input;
   t_output      binary_file;
+  t_parsing     parsing;
 }               t_data;
 
 int open_input(int argc, char **argv, t_input *input_data);
 int create_binary_file(t_input input);
 
 int parsing_input(t_data *data);
+char	*ft_strtrim_comment(char const *s, char comment_char);
 
 int generate_binary_code(t_data *data);
 
--- ASM_/src/parsing.c
+++ ASM_/src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: vomnes <vomnes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 16:58:00 by vomnes            #+#    #+#             */
-/*   Updated: 2017/04/27 19:46:11 by vomnes           ###   ########.fr       */
+/*   Updated: 2017/05/02 13:28:58 by vomnes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,25 @@ int		ft_isprint_nospace(int c)
 		return (0);
 }
 
-void check_header_content(char *clean_line, const char *cmd_string,\
-unsigned int content_len)
+void check_header_content(const char *cmd_string, unsigned int content_len,
+t_parsing *parsing)
 {
     char *name;
     int index_open;
     int index_close;
 
-    if (ft_strncmp(clean_line, cmd_string, ft_strlen(cmd_string)) == 0)
+    if (ft_strncmp(parsing->clean_line, cmd_string, ft_strlen(cmd_string)) == 0)
     {
-        name = clean_line + ft_strlen(cmd_string);
+        name = parsing->clean_line + ft_strlen(cmd_string);
         index_open = ft_charindex(name, '\"');
         if (ft_isstrn_ctype(name, ft_isprint_nospace, index_open))
         {
-            ft_printf("Syntax error before content %s - Line %d\n", cmd_string, 0);
+            ft_printf("Syntax error before content of %s - Line %d\n", cmd_string, parsing->line_nb);
             exit (-1);
         }
         if (ft_nb_occ(name, '\"') != 2)
         {
-            ft_printf("Syntax error - %s - Line %d\n", cmd_string, 0);
+            ft_printf("Syntax error - %s - Line %d\n", cmd_string, parsing->line_nb);
             exit (-1);
         }
         index_close = ft_charindex(name + index_open + 1, '\"');
@@ -48,29 +48,59 @@ unsigned int content_len)
             cmd_string, index_close - content_len, content_len);
             exit (-1);
         }
-        if (ft_isstr_ctype(name + (index_open + index_close + 2), ft_isprint))
+        if (ft_isstr_ctype(name + (index_open + index_close + 2), ft_isprint_nospace))
         {
-            ft_printf("Syntax error after content %s - Line %d\n", cmd_string, 0);
+            ft_printf("Syntax error after content %s - Line %d\n", cmd_string, parsing->line_nb);
             exit (-1);
         }
     }
+	if (ft_strncmp(parsing->clean_line, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)) == 0)
+		parsing->name_stocked = 1;
+	else if (ft_strncmp(parsing->clean_line, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)) == 0)
+		parsing->comment_stocked = 1;
 }
+
 int parsing_input(t_data *data)
 {
     char *line;
-    char *clean_line;
 
 	line = NULL;
-    clean_line = NULL;
+    data->parsing.clean_line = NULL;
+	data->parsing.line_nb = 0;
+	data->parsing.name_stocked = 0;
+	data->parsing.comment_stocked = 0;
 	while (get_next_line(data->input.fd, &line) > 0)
 	{
-        if (!(clean_line = ft_strtrim(line)))
+		data->parsing.line_nb++;
+        if (!(data->parsing.clean_line = ft_strtrim_comment(line, COMMENT_CHAR)))
         {
             ft_strdel(&line);
             return (-1);
         }
-        check_header_content(clean_line, NAME_CMD_STRING, PROG_NAME_LENGTH);
-        check_header_content(clean_line, COMMENT_CMD_STRING, COMMENT_LENGTH);
+		if (*data->parsing.clean_line == '\0')
+			continue ;
+		printf("%s\n", data->parsing.clean_line);
+        check_header_content(NAME_CMD_STRING, PROG_NAME_LENGTH, &data->parsing);
+        check_header_content(COMMENT_CMD_STRING, COMMENT_LENGTH, &data->parsing);
+		if (ft_strncmp(data->parsing.clean_line, NAME_CMD_STRING, ft_strlen(NAME_CMD_STRING)) != 0 &&
+		ft_strncmp(data->parsing.clean_line, COMMENT_CMD_STRING, ft_strlen(COMMENT_CMD_STRING)) != 0)
+		{
+			if (data->parsing.name_stocked == 0 && data->parsing.comment_stocked == 0)
+			{
+				ft_putendl_fd("Name and Comment undefined", 2);
+				exit (-1);
+			}
+			else if (data->parsing.comment_stocked == 0)
+			{
+				ft_putendl_fd("Comment undefined", 2);
+				exit (-1);
+			}
+			else if (data->parsing.name_stocked == 0)
+			{
+				ft_putendl_fd("Name undefined", 2);
+				exit (-1);
+			}
+		}
         ft_strdel(&line);
     }
     ft_strdel(&line);
