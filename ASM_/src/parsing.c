94e26548216bf1dba19f694a367f71ac620d2b90
Valentin OMNES <vomnes@student.42.fr>
2017-05-02 19:45:29 +0200

    Start parsing labels

--- ASM_/src/parsing.c
+++ ASM_/src/parsing.c
@@ -6,11 +6,45 @@
 /*   By: vomnes <vomnes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 16:58:00 by vomnes            #+#    #+#             */
-/*   Updated: 2017/05/02 14:49:34 by vomnes           ###   ########.fr       */
+/*   Updated: 2017/05/02 19:44:26 by vomnes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "asm.h"
+
+static int		ft_is_label_chars(int c)
+{
+	if (ft_strchr(LABEL_CHARS, (char)c) != NULL)
+		return (0);
+	else
+		return (1);
+}
+
+int parse_content(t_parsing *parsing)
+{
+	if (parsing->name_stocked == 1 && parsing->comment_stocked == 1)
+	{
+		int i;
+		char *content;
+
+		i = 0;
+		while (parsing->clean_line[i] != '\0')
+		{
+			if (parsing->clean_line[i] == ':')
+			{
+				if (!(content = ft_strndup(parsing->clean_line, ft_charindex(parsing->clean_line, ':'))))
+					return (-1);
+				break ;
+			}
+			i++;
+		}
+		if (ft_isstr_ctype(content, ft_is_label_chars) == 1)
+			;
+		else
+			ft_putendl_fd(content, 2);
+	}
+	return (0);
+}
 
 void init_struct_parsing(t_parsing *parsing)
 {
@@ -36,7 +70,8 @@ int parsing_input(t_data *data)
 			ft_strdel(&data->parsing.clean_line);
 			continue ;
 		}
-		printf("%s\n", data->parsing.clean_line);
+		// ft_printf("%s\n", data->parsing.clean_line);
+		parse_content(&(data->parsing));
 		parse_name_comment(&(data->parsing));
 		ft_strdel(&data->parsing.clean_line);
     }
