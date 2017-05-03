c1b3e7aad08e63f00d910d91fdef80a6307f62d0
Pierre DADY <pdady@e3r8p19.42.fr>
2017-05-03 19:27:42 +0200

    Update instruction ld

--- VM_/src/vm_ld.c
+++ VM_/src/vm_ld.c
@@ -1,17 +1,51 @@
 #include "vm.h"
 
+int					move_pc(t_process *process, int pc)
+{
+	int count;
+
+	count = 2;
+	if (process->instruction.first_type == T_DIR)
+		count += 4;
+	else if (process->instruction.first_type == T_IND)
+		count += 2;
+	else if (process->instruction.first_type == T_REG)
+		count += 1;
+	if (process->instruction.second_type == T_DIR)
+		count += 4;
+	else if (process->instruction.second_type == T_IND)
+		count += 2;
+	else if (process->instruction.second_type == T_REG)
+		count += 1;
+	pc += count;
+	return (pc);
+}
+
 void				vm_ld(t_process *process, t_vm *vm)
 {
 	unsigned int	pc;
 	int				first_param;
 	int				second_param;
 
-	pc = vm_read_from_register(process->pc);
+	first_param = 0;
+	second_param = 0;
+	pc = vm_read_register(process->pc);
 	vm_decode_parameter_byte(process, vm);
 	if (process->instruction.first_type == T_DIR)
-		first_param = vm_read_memory_int(vm, pc + 1);
+	{
+		first_param = vm_read_memory_int(vm, pc + 2);
+		second_param = MOD(pc + 6);
+		if (second_param > 0 && second_param < REG_NUMBER)
+			vm_store_in_register(&process->registers[second_param],
+								(pc + (first_param % IDX_MOD)));
+	}
 	else if (process->instruction.first_type == T_IND)
-		first_param = vm_read_memory_short(vm, pc + 1);
-	second_param = MOD(pc + 1);
-	vm_store_in_register(&process->registers[second_param], (PC + (first_param % IDX_MOD)));
+	{
+		first_param = vm_read_memory_short(vm, pc + 2);
+		second_param = MOD(pc + 4);
+		if (second_param > 0 && second_param < REG_NUMBER)
+			vm_store_in_register(&process->registers[second_param],
+								(pc + (first_param % IDX_MOD)));
+	}
+	vm_store_in_register(&process->pc, move_pc(process, pc));
 }
