6a0321b069946510db12ca11e8f31568b6ad8ba5
Pierre DADY <pdady@e2r6p17.42.fr>
2017-05-09 11:50:02 +0200

    Fixe bug for 4 players

--- VM_/src/vm_fill_memory_vm.c
+++ VM_/src/vm_fill_memory_vm.c
@@ -16,19 +16,6 @@ void				print_memory_dump(t_vm vm)
 	}
 }
 
-int					count_number_players(t_vm vm)
-{
-	int				i;
-	int				count;
-
-	i = 0;
-	count = 0;
-	while (++i < MAX_PLAYERS)
-		if (vm.players[i].number != 0)
-			count++;
-	return (count);
-}
-
 void				vm_fill_memory(t_vm *vm, int no_player, size_t pos)
 {
 	size_t		i;
@@ -47,17 +34,17 @@ void				vm_fill_memory_vm(t_vm *vm)
 	unsigned long	tmp;
 	int				no_player;
 
-	size = MEM_SIZE;
+	size = 0;
 	no_player = 2;
-	number_players = count_number_players(*vm);
-	tmp = size / number_players;
+	number_players = vm->nb_players;
+	tmp = MEM_SIZE / number_players;
 	vm_fill_memory(vm, 1, 0);
-	while (no_player < MAX_PLAYERS)
+	while (no_player <= MAX_PLAYERS)
 	{
 		if (vm->players[no_player].number != 0)
 		{
-			vm_fill_memory(vm, no_player, tmp);
-			tmp = tmp + tmp;
+			size = size + tmp;
+			vm_fill_memory(vm, no_player, size);
 		}
 		no_player++;
 	}
