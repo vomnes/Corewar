3c650adae84ba329dba1c67676eab3c20b96861a
Valentin OMNES <vomnes@student.42.fr>
2017-05-17 22:25:56 +0200

    Update test script vm

--- test_script/test_vm/test_vm.sh
+++ test_script/test_vm/test_vm.sh
@@ -2,20 +2,31 @@
 
 corewar_cor1_cor2()
 {
-    ./asm $1.s
-    ./asm $2.s
-    echo "\x1b[38;5;214m--> Corewar $1\033[0m"
+    echo "\033[1;33m$1 vs $2 :\033[0m\c "
+    ../../ASM_/asm $1.s > /dev/null
+    ../../ASM_/asm $2.s > /dev/null
+    echo "#\c"
     ./cor/corewar -v 31 $1.cor $2.cor > output1
-    echo "\x1b[38;5;214m--> Corewar $2\033[0m"
+    echo "#\c"
     ./cor_zaz/corewar -v 31 $1.cor $2.cor > output2
     diff output1 output2 > output_diff
     if [[ -s output_diff ]]
-    then echo "\033[1;33m$it: \033[0;31mFAILURE\033[0m"
+    then echo "\033[0;32mFAILURE\033[0m"
     diff output1 output2 > ./diff/$1$2_diff
-    else echo "\033[1;33m$it: \033[0;32mSUCCESS\033[0m"
+    else echo "\033[0;32mSUCCESS\033[0m"
     fi
 }
 
 rm -r diff
 mkdir diff
-corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/ex
+corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/toto
+# corewar_cor1_cor2 ../../champs/car ../../champs/gagnant
+corewar_cor1_cor2 ../../champs/barriere ../../champs/car
+corewar_cor1_cor2 ../../champs/jumper ../../champs/ex
+corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/jumper
+corewar_cor1_cor2 ../../champs/lde ../../champs/maxidef
+corewar_cor1_cor2 ../../champs/mortel ../../champs/mat
+corewar_cor1_cor2 ../../champs/toto ../../champs/slider2
+corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/maxidef
+
+#car gagnant barriere jumper lde maxidef mat mortel slider2 toto"
