f61c1ac7fc853267c0ed55c52e36266551307b30
Valentin OMNES <vomnes@student.42.fr>
2017-05-19 19:46:51 +0200

    Loading % test_vm.sh

--- test_script/test_vm/test_vm.sh
+++ test_script/test_vm/test_vm.sh
@@ -9,40 +9,56 @@ corewar_cor1_cor2()
     echo " [2] \c"
     ./cor_zaz/corewar -v 31 $1.cor $2.cor > output2
     echo "[1]\c"
-    ../../VM_/corewar -v 31 $1.cor $2.cor > output1
+    total=$(ls -la | grep "output2" | awk '{print $5}')
+    ../../VM_/corewar -v 31 $1.cor $2.cor > output1 &
+    pid=$!
+    trap "kill $pid 2> /dev/null" EXIT
+    # While copy is running...
+    echo ""
+    while kill -0 $pid 2> /dev/null; do
+        # test=ls -la | grep "output1" | awk '{print $5}'
+        size2=$(ls -la | grep "output1" | awk '{print $5}')
+        update=$(echo "scale=2;100 * $size2 / $total" | bc)
+        printf "\rLoading [%.f%%]" $update
+    done
+    trap - EXIT
     echo " >>\c"
     diff output1 output2 > output_diff
     if [[ -s output_diff ]]
     then echo "\033[0;31m FAILURE\033[0m"
-    touch ./diff/$count.diff
-    diff output1 output2 > ./diff/$count.diff
     diff output1 output2
-    cat output1 > ./diff/$count.output1
-    cat output2 > ./diff/$count.output2
-    let "count++"
     else echo "\033[0;32m SUCCESS\033[0m"
+    rm output1
+    rm output2
+    rm output_diff
     fi
 }
 
-rm -r diff
-mkdir diff
+make -C ../../ASM_/
 make -C ../../VM_/
-corewar_cor1_cor2 ../../champs/jumper ../../VM_/amric_player
-corewar_cor1_cor2 ../../champs/jumper ../../VM_/l
-corewar_cor1_cor2 ../../VM_/amric_player ../../VM_/l
-corewar_cor1_cor2 ../../VM_/l ../../champs/jumper
-# # corewar_cor1_cor2 ../../champs/Car ../../VM_/l
-# corewar_cor1_cor2 ../../champs/mortel ../../VM_/l
-# corewar_cor1_cor2 ../../champs/toto ../../VM_/l
-# corewar_cor1_cor2 ../../VM_/l ../../VM_/toto
-# corewar_cor1_cor2 ../../champs/car ../../champs/gagnant
-# corewar_cor1_cor2 ../../champs/jumper ../../champs/car
-# corewar_cor1_cor2 ../../champs/jumper ../../champs/ex
-corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../VM_/a
-corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/jumper
-corewar_cor1_cor2 ../../champs/slider2 ../../champs/maxidef
-corewar_cor1_cor2 ../../champs/mortel ../../champs/maxidef
-corewar_cor1_cor2 ../../champs/toto ../../champs/slider2
-corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/maxidef
+for one in $@
+do
+    for two in $@
+    do
+        corewar_cor1_cor2 ${one%.*} ${two%.*}
+    done
+done
+# corewar_cor1_cor2 ../../champs/jumper ../../VM_/amric_player
+# corewar_cor1_cor2 ../../champs/jumper ../../VM_/l
+# corewar_cor1_cor2 ../../VM_/amric_player ../../VM_/l
+# corewar_cor1_cor2 ../../VM_/l ../../champs/jumper
+# # # corewar_cor1_cor2 ../../champs/Car ../../VM_/l
+# # corewar_cor1_cor2 ../../champs/mortel ../../VM_/l
+# # corewar_cor1_cor2 ../../champs/toto ../../VM_/l
+# # corewar_cor1_cor2 ../../VM_/l ../../VM_/toto
+# # corewar_cor1_cor2 ../../champs/car ../../champs/gagnant
+# # corewar_cor1_cor2 ../../champs/jumper ../../champs/car
+# # corewar_cor1_cor2 ../../champs/jumper ../../champs/ex
+# corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../VM_/a
+# corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/jumper
+# corewar_cor1_cor2 ../../champs/slider2 ../../champs/maxidef
+# corewar_cor1_cor2 ../../champs/mortel ../../champs/maxidef
+# corewar_cor1_cor2 ../../champs/toto ../../champs/slider2
+# corewar_cor1_cor2 ../../champs/Octobre_Rouge_V4.2 ../../champs/maxidef
 
 #car gagnant -barriere jumper lde maxidef mat mortel slider2 toto"
