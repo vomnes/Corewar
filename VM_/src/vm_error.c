004cdad3222acbc254c51ff0cc64bb5883eb441f
Amric TRUDEL <atrudel@e2r1p6.42.fr>
2017-05-22 18:36:08 +0200

    no real change made

--- VM_/src/vm_error.c
+++ VM_/src/vm_error.c
@@ -6,7 +6,7 @@
 /*   By: atrudel <atrudel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/18 11:15:12 by atrudel           #+#    #+#             */
-/*   Updated: 2017/05/18 11:15:27 by atrudel          ###   ########.fr       */
+/*   Updated: 2017/05/22 18:35:20 by atrudel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,5 @@ void	vm_error_exit(t_vm *vm, char *message)
 {
 	ft_putendl_fd(message, 2);
 	vm_delete_data(vm);
-	(void)vm;
 	exit(-1);
 }
