1ec45b6e09f84fcf1b6a9de1b2e76dd07ab47170
Pierre DADY <pdady@e2r2p5.42.fr>
2017-05-22 14:38:24 +0200

    Norme corewar.c : OK

--- VM_/src/corewar.c
+++ VM_/src/corewar.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   corewar.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdady <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/05/22 14:29:08 by pdady             #+#    #+#             */
+/*   Updated: 2017/05/22 14:30:33 by pdady            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "vm.h"
 
-int			print_usage(void)
+int				print_usage(void)
 {
 	ft_printf("Usage: ./corewar [-a -vis -d N -v N] <champion1.cor> <...>\n");
 	ft_printf("\t-a\t: Prints output from 'aff' (Default is to hide it)\n");
@@ -17,10 +29,10 @@ several\n");
 	return (0);
 }
 
-void		check_position_players(t_vm vm)
+void			check_position_players(t_vm vm)
 {
-	int i;
-	int a;
+	int			i;
+	int			a;
 
 	i = 0;
 	a = 0;
@@ -39,15 +51,36 @@ void		check_position_players(t_vm vm)
 	}
 }
 
-int main(int ac, char **av)
+void			go_fight(t_vm *vm, WINDOW *windows[4])
+{
+	int			ret;
+	t_player	*winner;
+
+	ret = 1;
+	vm_init(vm);
+	check_position_players(*vm);
+	vm_fill_memory_vm(vm);
+	vm_introduce_contestants(vm);
+	while (ret == 1)
+	{
+		(ret == 1 && vm->pause != 1) ? (ret = vm_do_one_cycle(vm)) : 1;
+		(vm->vis == 1) ? display_all_windows(vm, windows, ret) : 1;
+	}
+	if ((winner = vm_get_player(vm, vm->last_live_player_no)))
+		ft_printf("Contestant %d, \"%s\", has won !\n", winner->number,
+		winner->name);
+	else
+		ft_printf("No player has won because no live was well performed\n");
+	(vm->vis == 1) ? endwin() : 1;
+	vm_delete_data(vm);
+}
+
+int				main(int ac, char **av)
 {
 	t_vm		vm;
 	char		ret;
-	t_player	*winner;
 	WINDOW		*windows[4];
-	int entry;
 
-	entry = 0;
 	ret = 1;
 	ft_memset(&vm, 0, sizeof(t_vm));
 	if (ac < 2)
@@ -56,20 +89,7 @@ int main(int ac, char **av)
 	vm.dumps = -1;
 	av = vm_parse_line_command(av, &vm);
 	(vm.vis == 1) ? init_windows(windows) : 1;
-	vm_init(&vm);
-	check_position_players(vm);
-	vm_fill_memory_vm(&vm);
-	vm_introduce_contestants(&vm);
-	while (ret == 1)
-	{
-		(ret == 1 && vm.pause != 1) ? (ret = vm_do_one_cycle(&vm)) : 1;
-		(vm.vis == 1) ? display_all_windows(&vm, windows, ret) : 1;
-	}
-	if ((winner = vm_get_player(&vm, vm.last_live_player_no)))
-		ft_printf("Contestant %d, \"%s\", has won !\n", winner->number,
-		winner->name);
-	else
-		ft_printf("No player has won because no live was well performed\n");
+	go_fight(&vm, windows);
 	(vm.vis == 1) ? endwin() : 1;
 	vm_delete_data(&vm);
 	return (0);
